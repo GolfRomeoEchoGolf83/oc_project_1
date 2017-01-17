b4b61caa7e0f967dbf538f1e44d5e260c06b67c6
GolfRomeoEchoGolf83 <gregdecroix@gmail.com>
2017-01-17 16:04:15 +0100

    adding wp-content files

---
+++ themes/full-frame-child/script.js
@@ -0,0 +1,13 @@
+// au clic ouvre un formulaire : nom et envoi à la mairie
+// gestion de l'évènement clic
+// popup formulaire
+// envoi vers la mairie
+
+
+// -------------------------------
+// la dernière actualité est poussée à la page d'accueil "les dernières activités du village"
+
+// prend le dernier article dont la catérogie est "actualité"
+// redirige vers la page d'accueil
+
+// -------------------------------
